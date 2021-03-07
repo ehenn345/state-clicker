f8b72987c177bdab246f184c4859c8fc42c6ee61
Erin <westonerin1@gmail.com>
2021-03-07 10:41:38 -0500

    create clicker state and render number

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 
 class Clicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 1 };
+  }
   render() {
-    return <h1>Clicker</h1>;
+    return (
+      < div >
+        <h1>Number is: {this.state.num}</h1>
+        <button onClick={this.genRandom}>random number</button>
+      </div >
+    );
   }
 }
 
